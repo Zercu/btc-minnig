797e90dc36bd1e48b61cbaaa8d28eadd40c2b8e4
Zercu <dekakiran540@gmail.com>
2024-08-25 16:23:38 +0530

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,19 +1,11 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-
-# Configuration details from your bitcoin.conf
-rpc_user = "your_rpc_username"
-rpc_password = "your_rpc_password"
-rpc_host = "127.0.0.1"
-rpc_port = "8332"
+from config import RPC_USER, RPC_PASSWORD, RPC_HOST, RPC_PORT
 
 # Connect to Bitcoin Core RPC
-rpc_url = f"http://{rpc_user}:{rpc_password}@{rpc_host}:{rpc_port}"
+rpc_url = f"http://{RPC_USER}:{RPC_PASSWORD}@{RPC_HOST}:{RPC_PORT}"
 client = AuthServiceProxy(rpc_url)
 
 def get_new_address(label=""):
-    """
-    Generate a new Bitcoin address.
-    """
     try:
         address = client.getnewaddress(label)
         return address
@@ -22,9 +14,6 @@ def get_new_address(label=""):
         return None
 
 def get_balance():
-    """
-    Get the balance of the wallet.
-    """
     try:
         balance = client.getbalance()
         return balance
@@ -33,12 +22,7 @@ def get_balance():
         return None
 
 def send_to_address(address, amount):
-    """
-    Send Bitcoin to a specific address.
-    """
     try:
         txid = client.sendtoaddress(address, amount)
         return txid
-    except JSONRPCException as e:
-        print(f"Error sending to address: {e}")
-        return None
+    except JSON
